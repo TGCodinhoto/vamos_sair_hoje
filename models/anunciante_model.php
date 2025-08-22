20200c61fad53e430e38e99e0c1f130c7b165cea
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-08-22 17:36:36 -0300

    feat: adiciona o model de anunciante

---
+++ models/anunciante_model.php
@@ -0,0 +1,23 @@
+<?php
+class AnuncianteModel
+{
+    private $conexao;
+
+    public function __construct($conexao)
+    {
+        $this->conexao = $conexao;
+    }
+
+    public function criar($dados)
+    {
+        $stmt = $this->conexao->prepare("
+        INSERT INTO anunciante (publicacaoid, anunciantebanner)
+        VALUES (:publicacaoid, :banner)
+    ");
+
+        return $stmt->execute([
+            ':publicacaoid' => $dados['publicacaoid'],
+            ':banner' => $dados['banner']
+        ]);
+    }
+}
