71d37b87f190067407a51b2ee41aa2301216ac28
Thiago Gravonski <thiagogravonski@hotmail.com>
2025-08-25 14:57:39 -0300

    bg footer

--- ui/footer.php
+++ ui/footer.php
@@ -1,7 +1,7 @@
 
 
 <!-- Footer  -->
-<footer class="bg-[#333] text-white text-center py-5 mt-10">
+<footer class="bg-[#1B3B57] text-white text-center py-5 mt-10">
     <div class="max-w-7xl mx-auto px-4">
         <p>&copy; 2025 Vamos Sair Hoje. Todos os direitos reservados.</p>
     </div>
