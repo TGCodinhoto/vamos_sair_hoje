d5862d70e71e329e2c7ba67469083b1edce5d06f
Matheus Vinicius <matheus.v.cruz@cogna.com.br>
2025-09-09 16:37:49 -0300

    fix: logger error

--- views/login_process.php
+++ views/login_process.php
@@ -8,8 +8,6 @@ require_once '../config_env.php';
 SessionManager::iniciarSessao();
 
 try {
-    Logger::info("=== Iniciando tentativa de login ===");
-    Logger::debug("Email: " . ($_POST['email'] ?? 'não fornecido'));
 
     // Verifica se os campos foram enviados
     if (!isset($_POST['email']) || !isset($_POST['senha'])) {
@@ -61,29 +59,21 @@ try {
     }
 
     if ($resultado['sucesso']) {
-        Logger::info("Login bem-sucedido para o usuário: " . $email);
-        Logger::info("Tipo de usuário: " . $resultado['tipo']);
-        Logger::info("Sessão após login: " . print_r($_SESSION, true));
         
         // Redireciona com base no tipo de usuário
         if ($resultado['tipo'] == 2) { // Estabelecimento
-            Logger::info("Redirecionando para formulários (tipo 2)");
             header('Location: navegacao_forms.php');
         } else if ($resultado['tipo'] == 1) { // Administrador
-            Logger::info("Redirecionando para formulários (tipo 1)");
             header('Location: navegacao_forms.php');
         } else { // Usuário comum
-            Logger::info("Redirecionando para index (tipo 3)");
             header('Location: ../index.php');
         }
     } else {
-        Logger::error("Falha no login: " . ($resultado['mensagem'] ?? 'Motivo desconhecido'));
         $_SESSION['erro'] = $resultado['mensagem'] ?? 'Email ou senha incorretos';
         $_SESSION['email'] = $email; // Preserva o email para conveniência
         header('Location: login.php');
     }
 } catch (Exception $e) {
-    Logger::error("Erro no login: " . $e->getMessage());
     $_SESSION['erro'] = $e->getMessage();
     $_SESSION['email'] = $email ?? ''; // Preserva o email mesmo em caso de erro
     header('Location: login.php');
